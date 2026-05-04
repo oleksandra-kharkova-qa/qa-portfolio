77891f9f2f0d31f408cde2328e940ae615485c12
oleksandra-kharkova-qa <oleksandra.kharkova@gmail.com>
2026-05-04 21:48:17 +0300

    add negative login tests - validation and bypass browser validation

--- tests/login-functional.spec.ts
+++ tests/login-functional.spec.ts
@@ -3,7 +3,7 @@ import { users } from './data/users';
 import { LoginPage } from './pages/LoginPage';
 
 for (const user of users) {
-test (`User can login as ${user.name}`, async ({ page }) => {
+test (`user can login as ${user.name}`, async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
     await loginPage.login(user.email, user.password);
--- tests/login-negative.spec.ts
+++ tests/login-negative.spec.ts
@@ -1,14 +1,38 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "./pages/LoginPage";
-test.describe("Login negative scenarios", () => {
-    test("Email field is required", async ({ page }) => {
+
+test.describe("login negative scenarios", () => {
+    test("email field is required", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
     await loginPage.fieldIsRequired("email");
 });
-test("Password field is required", async ({ page }) => {
+test("password field is required", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
     await loginPage.fieldIsRequired("password");
 });
+test('cannot login with empty email', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await page.evaluate (() => {
+        document.querySelector('[data-qa="login-email"]')?.removeAttribute('required');
+         });
+    await loginPage.login('', 'somepassword');
+    await expect(page).toHaveURL(/login/);
+});
+});
+
+
+test.describe("login form - bypass browser validation", () => {
+    test('cannot login with both fields empty', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await page.evaluate (() => {
+        document.querySelector('[data-qa="login-email"]')?.removeAttribute('required');
+        document.querySelector('[data-qa="login-password"]')?.removeAttribute('required');
+         });
+    await loginPage.login('', '');
+    await expect(page).toHaveURL(/login/);
+  });
 });
